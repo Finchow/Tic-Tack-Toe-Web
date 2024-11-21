9c7179ef85542f3d697c3f5af4947703061a9e1e
Oscar White <oscar.white361@icloud.com>
2024-11-21 15:39:27 +0000

    temp

---
+++ Tic-Tack-Toe-Web/Tic-Tack-Toe-Web.Client/Classes/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tack_Toe_Web.Client.Classes;
+
+public class Grid
+{
+    public Grid(int width, int height)
+    {
+        Tiles = CrateGrid(width, height);
+        Width = width;
+        Height = height;
+    }
+
+    public List<Tile> Tiles { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    private static List<Tile> CrateGrid(int x, int y)
+    {
+        var tileNum = x * y;
+
+        List<Tile> gameGrid = [];
+
+        for (int i = 0; i < tileNum; i++)
+        {
+            Tile tile = new();
+            gameGrid.Add(tile);
+        }
+
+        return gameGrid;
+    }
+}
---
+++ Tic-Tack-Toe-Web/Tic-Tack-Toe-Web.Client/Classes/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tack_Toe_Web.Client.Classes;
+
+public class Tile
+{
+
+    public Tile()
+    {
+        Value = "";
+    }
+
+    public string Value { get; set; }
+
+}
